c44e88064540facc50707056f2821d25e96da524
MJ Kim <myoungjun.kim@gmail.com>
2016-05-02 20:41:07 +0900

    Initial Commit

---
+++ DeviceStatus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeviceStatus
+//
+//  Created by MJ Kim on 2016. 5. 2..
+//  Copyright © 2016년 Swifter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
